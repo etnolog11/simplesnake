5ffe91a9911b3d2a5515133d0759c9bb0b53be1d
etnolog11 <volkovvladyslav.off@gmail.com>
2023-04-11 22:59:45 +0200

    Added maps
    The time complexity of this is humongous as the snake gets to be 8 elements with around 150 obstacles it becomes unplayable. The controls work is unresponsive

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
 from tkinter import *
-import time
 import random
 
 is_running = False
+lvl_slc = 0
 
 
 def board_setup(map_type):
+    global is_running, snake, apple, obstacles, was_changed
+    for button in buttons:
+        button.place_forget()
     if map_type == 0:
-        global snake, apple, obstacles, was_changed
-        was_changed = False
-        field.delete("all")
-        apple = [14, 11]
-        obstacles = [[1, 3], [6, 8], [27, 15]]
+        obstacles = []
         snake = [[17, 17, 1, 1], [17, 18, 0, 1], [17, 19, 2, 1]]
-        for i in range(36):
-            field.create_line((i - 1) * 20, 0, (i - 1) * 20, 700, tags='field')
-        for i in range(36):
-            field.create_line(0, (i - 1) * 20, 700, (i - 1) * 20, tags='field')
-        field_refresh()
+    elif map_type == 1:
+        obstacles = []
+        for i in range(1,36):
+             obstacles.append([i,1])
+             obstacles.append([i, 35])
+        for i in range(2,35):
+             obstacles.append([1,i])
+             obstacles.append([35, i])
+        snake = [[17, 17, 1, 1], [17, 18, 0, 1], [17, 19, 2, 1]]
+    elif map_type == 2:
+        obstacles = []
+        for i in range(1, 36):
+            obstacles.append([i, 1])
+            obstacles.append([i, 35])
+        for i in range(2, 35):
+            obstacles.append([1, i])
+            obstacles.append([35, i])
+        for i in range(3,34):
+            if i!=17:
+               obstacles.append([17,i])
+        for i in range(3, 34):
+            obstacles.append([i, 17])
+        snake = [[2, 17, 1, 1], [2, 18, 0, 1], [2, 19, 2, 1]]
+    is_running = 1
+    was_changed = False
+    apple = apple_find()
+    field.delete("all")
+    field_refresh()
+    game_step()
+    for i in range(36):
+        field.create_line((i - 1) * 20, 0, (i - 1) * 20, 700, tags='field')
+    for i in range(36):
+        field.create_line(0, (i - 1) * 20, 700, (i - 1) * 20, tags='field')
+
 
 
 def lose_screen():
@@ -26,6 +54,7 @@ def lose_screen():
     field.delete('all')
     field.create_text(350, 350, font="Calibri",
                       text=f"You have scored {(len(snake) - 4)} points. Press Space to restart")
+    level_selection()
 
 
 def game_step():
@@ -60,8 +89,7 @@ def game_step():
         iterator = snake.copy()
         iterator.remove(head)
         for segment in iterator:
-            if head[:2] == segment[
-                           :2]:  # not (segment[2]==2 and not(head[0]!=segment[0]or head[1]!=segment[1])): #not(segment[2]==2 and head[0]==apple[0] and head[1]==apple[1]):
+            if head[:2] == segment[:2]:
                 if segment[2] != 2:
                     bitesitself = True
                 elif apple[:2] == head[:2]:
@@ -87,18 +115,22 @@ def game_step():
                 field.create_text(350, 350, font="Calibri",
                                   text=f"You have won. Press Space to restart")
             else:
-                free_place = []
-                for i in range(1, 1226):
-                    free_place.append(i)
-                for element in snake:
-                    free_place.remove(element[0] + (element[1] - 1) * 35)
-                for element in obstacles:
-                    free_place.remove(element[0] + (element[1] - 1) * 35)
-                a = free_place[random.randint(0, len(free_place) - 1)]
-                apple[0] = a % 35 + 1
-                apple[1] = a // 35 + 1
+                apple = apple_find()
         field_refresh()
         screen.after(450, lambda: [game_step()])
+
+
+
+def apple_find():
+    free_place = []
+    for i in range(1, 1226):
+        free_place.append(i)
+    for element in snake:
+        free_place.remove(element[0] + (element[1] - 1) * 35)
+    for element in obstacles:
+        free_place.remove(element[0] + (element[1] - 1) * 35)
+    a = free_place[random.randint(0, len(free_place) - 1)]
+    return [a % 35 + 1,a // 35 + 1]
 
 
 def field_refresh():
@@ -117,13 +149,6 @@ def field_refresh():
                                fill="#000000", tags='field')
 
 
-def game_start(event):
-    global is_running
-    if is_running == 0:
-        is_running = 1
-        board_setup(0)
-        game_step()
-
 
 def Next_turn(event):
     global was_changed
@@ -134,16 +159,26 @@ def Next_turn(event):
     was_changed = True
 
 
+def level_selection():
+    global buttons
+    lvl0 = Button(master=screen, text="lvl0", width=10, command=lambda :board_setup(0))
+    lvl1 = Button(master=screen, text="lvl1", width=10, command=lambda :board_setup(1))
+    lvl2 = Button(master=screen, text="lvl2", width=10, command=lambda :board_setup(2))
+    buttons = [lvl0, lvl1, lvl2]
+    for button in buttons:
+        button.place(x=350,y=(buttons.index(button)+1)*50,anchor="center")
+
+
 if __name__ == "__main__":
     screen = Tk()
     screen.resizable(False, False)
     screen.geometry("700x700")
     screen.title("Snake 1.0")
-    screen.bind("<space>", game_start)
     screen.bind("<Up>", Next_turn)
     screen.bind("<Right>", Next_turn)
     screen.bind("<Down>", Next_turn)
     screen.bind("<Left>", Next_turn)
     field = Canvas(screen, height=700, width=700)
     field.place(x=0, y=0)
+    level_selection()
     screen.mainloop()
